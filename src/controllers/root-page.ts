193bef4c22ac9a9e591474fe07f4d1b9713e232a
Safin-Ali <safin.ali.7205@gmail.com>
2024-02-01 03:51:20 +0600

    feat: :wheelchair: awake app instance in limition sleep server

--- src/controllers/root-page.ts
+++ src/controllers/root-page.ts
@@ -1,5 +1,26 @@
-import { routeHandler } from '@utilities/common-utilities';
+import { awake_key, self_domain } from '@config/env-var';
+import logger from '@utilities/color-logger';
+import { routeHandler, userAgent } from '@utilities/common-utilities';
+import inDevMode from '@utilities/development-mode';
+import nodeFetch from 'node-fetch';
 
 export const rootRouteHandler = routeHandler((_req,res) =>{
-	res.send(`<h1>this is root page </h1>`);
+	// awake app instance after 13 minutes later
+	if(_req.headers['awake-key'] === awake_key!) {
+		setTimeout(() => {
+			nodeFetch(self_domain!,{
+				method:'HEAD',
+				headers:{
+					'User-Agent':userAgent,
+					'awake-key':awake_key!
+				}
+			})
+			inDevMode(() => {
+				logger.process('Awaked');
+			})
+			res.send('Awaked')
+		},780*1000)
+	} else {
+		res.send('Welcome MOVIES4U SERVER');
+	}
 });
